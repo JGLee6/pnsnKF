2c20add8bcf0776a0f188e416854e5d598cfa68b
JGLee6 <j.greendeer.lee@gmail.com>
2017-05-27 15:07:11 -0700

    Generates ARMA from pole-zero response, transition matrices method

--- src/seismic.py
+++ src/seismic.py
@@ -5,6 +5,8 @@ import datetime as dt
 import matplotlib.pyplot as plt
 import scipy.signal as sig
 import pykalman as pyk
+import re
+import numpy.polynomial.polynomial as poly
 
 # plt.interactive(False)
 
@@ -21,30 +23,105 @@ class SeismicReader(object):
         # self.report = self.client.get_stations(t2, t1, network='UW', level='response', latitude=47.6603, longitude=-122.3031,
         #                                 maxradius=.05)
 
-        self.inventory = self.client.get_stations(t2, t1, network='UW', level='response', latitude=47.695,
-                                               longitude=-127.955, maxradius=2.3)
-
-        print(self.inventory)
-
-        # self.inventory.plot()
+        self.inventory = self.client.get_stations(t2, t1, network='UW', 
+                                                  level='response', 
+                                                  latitude=47.695,
+                                                  longitude=-127.955, 
+                                                  maxradius=2.3)
 
         # Attempt at getting all waveforms from all of the stations in some radius:
         channels = self.inventory.get_contents()['channels']
-        print(channels)
-        # bulk = [channel.split('.') for channel in channels]
-        # for item in bulk:
-        #     item.append(str(t2))
-        #     item.append(str(t1))
+        #print(channels)
+        bulk = [channel.split('.') for channel in channels]
+        for item in bulk:
+            item.append(t1)
+            item.append(t2)
         #
         # print(bulk)
         #
-        # self.waveForms = []
-        # for k, item in enumerate(bulk):
-        #     if k > 2:
-        #         break
-        #     print(item)
-        #     self.waveForms.append(np.copy(self.client.get_waveforms(*item)[0]))
-        #     print(self.waveForms[-1])
+        self.waveForms = []
+        self.ARMA = []
+        for k, item in enumerate(bulk):
+            if k > 2:
+                break
+            self.waveForms.append(np.copy(self.client.get_waveforms(*item)[0]))
+            inv = self.client.get_stations(t2,t1,network=item[0],
+                                           station=item[1],location=item[2],
+                                           channel=item[3],level='response')
+            self.ARMA.append(self.get_pzg(inv))
+            
+    def get_pzg(self):
+        """
+        Takes output of zero,pole query as string. Splits into appropriate 
+        roots and then converts to coefficients of a polynomial.
+        
+        Returns
+        -------
+        AR : ndarray
+            List of AR coefficients normalized so first AR coefficient is 1.
+        MA : ndarray
+            List of MA coefficients normalized so first AR coefficient is 1.
+        """
+        _,zeros,poles,K = re.split('ZEROS|POLES|CONSTANT',self.report.reponse.get_sacpz())
+        zeros = zeros.split()
+        # first value is number of zeros, get roots as complex floats
+        nZ = int(zeros[0])
+        zVals = np.zeros(nZ,dtype='complex')
+        for k in range(nZ):
+            zVals[k] = float(zeros[2*k+1])+1j*float(zeros[2*k+2])
+        poles = poles.split()
+        # first value is number of poles, get roots as complex floats
+        nP = int(poles[0])
+        pVals = np.zeros(nP,dtype='complex')
+        for k in range(nP):
+            pVals[k] = float(poles[2*k+1])+1j*float(poles[2*k+2])
+        # only single value for gain coefficient
+        K = float(K.split()[0])
+        
+        # Multiply out roots of polynomials to get coefficients of ARMA model
+        ARp = poly.polyfromroots(pVals)[::-1]
+        MAq = poly.polyfromroots(zVals)[::-1]
+        p = len(ARp)
+        q = len(MAq)
+        # Scale so that coefficient on largest order pole coefficient is 1
+        # and scale inputs coefficients (zeros) by gain (conversion factor)
+        MAq *= K
+        
+        # Because the transition matrices and covariance matrices depend on 
+        # the number of AR and MA coefficients, we'll extend the outputs as 
+        # zeros beyond the last value
+        if p > q:
+            AR = ARp
+            MA = np.zeros(p,dtype='complex')
+            MA[:q] = MAq
+        elif q > p:
+            MA = MAq
+            AR = np.zeros(q,dtype='complex')
+            AR[:p] = ARp
+        else:
+            AR = ARp
+            MA = MAq
+        
+        return AR, MA
+    
+    def matrices(self):
+        """
+        Calls get_pzg to find ARMA coefficients and then defines the transition 
+        matrix of the hidden state, the selection matrix of the hidden state, 
+        and the transition matrix of the observed state.
+        """
+        AR,MA = self.get_pzg()
+        r = len(AR)
+        H = np.zeros(r)
+        H[0] = 1.0
+        
+        G = np.zeros([r,r],dtype = 'complex')
+        G[:,0] = AR
+        np.fill_diagonal(G[:-1,1:], 1)
+        
+        R = np.copy(MA)
+        
+        return H, G, R
 
         # for data in waveForms:
         #     #wf = client.get_waveforms('UW','NOWS','','ENE',t2,t1)
