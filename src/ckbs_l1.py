37efaa4c337da79982a23697a062a0090f0c8c88
Abe <aengle2@uw.edu>
2017-05-30 22:53:16 -0700

    Add comments and readability to l1 solver.

--- src/ckbs_l1.py
+++ src/ckbs_l1.py
@@ -8,7 +8,8 @@ Created on Sun May  7 16:22:55 2017
 import numpy as np
 import scipy.linalg as la
 
-def ckbs_l1_affine(z,g,h,G,H,qinv,rinv,maxIter=10,epsilon=1e-2):
+
+def ckbs_l1_affine(z, g, h, G, H, qinv, rinv, maxIter=10, epsilon=1e-2):
     """
     Inputs
     ------
@@ -39,65 +40,74 @@ def ckbs_l1_affine(z,g,h,G,H,qinv,rinv,maxIter=10,epsilon=1e-2):
         Convergence parameter
     """
     # Hard-coded variables
-    # duality-gap
+    # initial duality gap
     mu = 100
-    # How close to boundary are we willing to go in one step
+
+    # fraction of true slope (aka c)
     gamma = .01
-    # Number of iterations of ?? (line search?)
+
+    # max num of backtracking tries
     maxKount = 18
-    # line search rejection criterion?
+
+    # control parameter for backtracking (aka gamma)
     beta = .5
+
     # corrector period, how many iterations before updating duality gap parameter
     cT = 3
-    
+
+    # things returned by the function
     info = []
+
     # Size of problem
-    n = np.shape(g)[-1]
-    N,m = np.shape(z)
-    
+    n = np.shape(g)[-1]  # should be r
+    N, m = np.shape(z)  # num time steps, dimension of observed data, expect m = 1 for us
+
     # Create vector of zero state values
-    xZero = np.zeros([N,n])
-    
+    xZero = np.zeros([N, n])
+
     # update D and A to not include measurements
     # compute tridiagonal elements of smoothing program
-    D,A = process_hess(G, qinv)
+    D, A = process_hess(G, qinv)
+
     # Compute gradient of loss wrt hidden state
     c = process_grad(xZero, g, G, qinv)
-    
+
     # Runs block-tridiagonal solver to find smoothed value
     # Gets Gauss-Newton step in linearization of observed state variable
     y = tridiag_solve_b(D, A, -c)
-    # block-tridiagonal multiplication with D as diaogonals, A as off-diags and
+
+    # block-tridiagonal multiplication with D as diagonals, A as off-diags and
     # y as vector
     cRec = blktridiag_mul(D, A, y)
+
     # Double checks that y is the solution to block-tridiag matrix(D,A)y = c
-    if np.linalg.norm(-c-cRec,ord=np.inf) > 1e-5:
+    if np.linalg.norm(-c - cRec, ord=np.inf) > 1e-5:
         print 'tridiagonal solver failed'
-        #return c, cRec
-    
+        # return c, cRec
+
     # Initiallize constraint variables and linearization of h(y)~h(x) + H*(x-y)
-    b = np.zeros([N,m])
-    r = np.zeros([N,m])
-    s = np.zeros([N,m])
-    crinv = np.zeros([N,m,m])   # Cholesky decomposition of rinv
-    B = np.zeros([N,m,n])
-    pPos = np.zeros([N,m])     # max(0, b + By)
-    pNeg = np.zeros([N,m])    # max(0,-b + By)
-    
+    b = np.zeros([N, m])
+    r = np.zeros([N, m])
+    s = np.zeros([N, m])
+    crinv = np.zeros([N, m, m])  # Cholesky decomposition of rinv
+    B = np.zeros([N, m, n])
+    pPos = np.zeros([N, m])  # max(0, b + By)
+    pNeg = np.zeros([N, m])  # max(0,-b + By)
+
     for k in xrange(N):
         crinv[k] = np.linalg.cholesky(rinv[k])  # prev.: crinv[k] = np.sqrt(rinv[k])
-        B[k] = np.dot(-crinv[k],H[k])
-        b[k] = np.dot(crinv[k], (z[k]-h[k]))
+        B[k] = np.dot(-crinv[k], H[k])
+        b[k] = np.dot(crinv[k], (z[k] - h[k]))
         # Version 1
-        temp = b[k] + np.dot(B[k],y[k])
+        temp = b[k] + np.dot(B[k], y[k])
         # No idea what this step is about (500???)
-        pPos[k] = 500 + max(0, temp) # initialization
+        pPos[k] = 500 + max(0, temp)  # initialization
         pNeg[k] = 500 + max(0, -temp)
-        r[k] = np.sqrt(2)*np.ones([1,m])
-        s[k] = np.sqrt(2)*np.ones([1,m])
-    
+        r[k] = np.sqrt(2) * np.ones([1, m])
+        s[k] = np.sqrt(2) * np.ones([1, m])
+
     # These should have been constructed to be zero
-    #if (np.min(s) < 0 or np.min(r) <= 0 or np.min(pPos) <= 0 or np.min(pNeg) <= 0):
+    # if (np.min(s) < 0 or np.min(r) <= 0 or np.min(pPos) <= 0 or np.min(pNeg) <= 0):
     #    print 'L1_affine: initial s, r, pPlus, pMinus not all positive'
     #    return
     # determine the value of y that solves the problem
@@ -108,70 +118,70 @@ def ckbs_l1_affine(z,g,h,G,H,qinv,rinv,maxIter=10,epsilon=1e-2):
         # Computes updates for KKT conditions
         F = kuhn_tucker_l1(mu, s, y, r, b, c, B, D, A, pPos, pNeg)
         # Computes newton descent step
-        dpPos, dpNeg, dr, ds, dy = newton_step_l1(mu,s,y,r,b,c,B,D,A,pPos,pNeg)
+        dpPos, dpNeg, dr, ds, dy = newton_step_l1(mu, s, y, r, b, c, B, D, A, pPos, pNeg)
         # Check that expressions involving updates of slack variables sum to 0
-        check_newton_l1(pNeg,dpNeg,s,ds,mu,1)
-        check_newton_l1(pPos,dpPos,r,dr,mu,2)
+        check_newton_l1(pNeg, dpNeg, s, ds, mu, 1)
+        check_newton_l1(pPos, dpPos, r, dr, mu, 2)
         # determine maximum allowable step factor lambda
-        ratio = np.array([dr, ds, dpPos, dpNeg])/ np.array([r, s, pPos, pNeg])
+        ratio = np.array([dr, ds, dpPos, dpNeg]) / np.array([r, s, pPos, pNeg])
         ratioMax = np.max(-ratio)
         # if ratioMax is not negative, pick from ratio not including ratioMax
         if ratioMax <= 0:
             lmbd = 1
         else:
-            rNeg = -1./ratio[ratio < 0]
+            rNeg = -1. / ratio[ratio < 0]
             maxNeg = np.min(rNeg)
-            lmbd = .99*np.min([maxNeg, 1])
-            
+            lmbd = .99 * np.min([maxNeg, 1])
+
         # Line Search
         ok = False
         kount = 0
-        lmbd = lmbd/beta
+        lmbd = lmbd / beta
         while (not ok) and (kount < maxKount):
             kount += 1
             lmbd *= beta
-            
+
             # update parameters with step size of lmbd
-            sNew = s + lmbd*ds
-            yNew = y + lmbd*dy
-            rNew = r + lmbd*dr
-            pPosNew = pPos + lmbd*dpPos
-            pNegNew = pNeg + lmbd*dpNeg
-            
+            sNew = s + lmbd * ds
+            yNew = y + lmbd * dy
+            rNew = r + lmbd * dr
+            pPosNew = pPos + lmbd * dpPos
+            pNegNew = pNeg + lmbd * dpNeg
+
             # Check for feasibility
-            if (np.min(sNew)<= 0) or (np.min(rNew)<=0) or (np.min(pPosNew)<=0) or (np.min(pNegNew)<=0):
+            if (np.min(sNew) <= 0) or (np.min(rNew) <= 0) or (np.min(pPosNew) <= 0) or (np.min(pNegNew) <= 0):
                 print 'L1_affine: program error, negative entries'
                 return
             # Compute the KKT conditions at the new values of y,p+,p-,s,r
             FNew = kuhn_tucker_l1(mu, sNew, yNew, rNew, b, c, B, D, A, pPosNew, pNegNew)
-            FMax = np.max(np.abs(F),0)
-            FNewMax = np.max(np.abs(FNew),0)
+            FMax = np.max(np.abs(F), 0)
+            FNewMax = np.max(np.abs(FNew), 0)
             # Check that all of the variables have decreased
             # XXX: I believe we want all, not any
-            ok = all(FNewMax <= (1-gamma*lmbd)*FMax)
+            ok = all(FNewMax <= (1 - gamma * lmbd) * FMax)
         if not ok:
-            df = np.max(F-FNew,0)
-            if all(df<= epsilon):
+            df = np.max(F - FNew, 0)
+            if all(df <= epsilon):
                 print 'L1_affine: line search failed'
-                #return
-        
+                # return
+
         s = sNew
         y = yNew
         r = rNew
         pPos = pPosNew
         pNeg = pNegNew
         # Compute the loss after the update and compare to inital state loss
-        VP   = l2l1_obj(y, z, g, h, G, H, qinv, rinv)
+        VP = l2l1_obj(y, z, g, h, G, H, qinv, rinv)
         Kxnu = l2l1_obj(xZero, z, g, h, G, H, qinv, rinv)
-        G1 = np.sum(r*pPos) + np.sum(s*pNeg)
+        G1 = np.sum(r * pPos) + np.sum(s * pNeg)
         converge = (G1 < np.min([Kxnu - VP, epsilon]))
         # Every third step is a corrector (That is update the mu parameter)
-        if (itr%cT == 0):
-            temp = np.sum(r*pPos) + np.sum(s*pNeg)
-            compMuFrac = temp/(2.*m*N)
-            mu = .1*compMuFrac
-        info.append([FMax, G1, Kxnu-VP, mu, kount])
-        
+        if (itr % cT == 0):
+            temp = np.sum(r * pPos) + np.sum(s * pNeg)
+            compMuFrac = temp / (2. * m * N)
+            mu = .1 * compMuFrac
+        info.append([FMax, G1, Kxnu - VP, mu, kount])
+
     return y, r, s, pPos, pNeg, info
 
 
@@ -181,11 +191,11 @@ def check_newton_l1(p, dp, s, ds, mu, num):
     slack variables to sum to zero. The variable 'num' just allows one to track 
     which expression fails.
     """
-    expr = np.linalg.norm(p*s + s*dp + p*ds - mu, ord = np.inf)
-    if (np.abs(expr)>1e-4):
-        print 'L1_affine: Newton Solver not working, expr',num,' not 0'
+    expr = np.linalg.norm(p * s + s * dp + p * ds - mu, ord=np.inf)
+    if (np.abs(expr) > 1e-4):
+        print 'L1_affine: Newton Solver not working, expr', num, ' not 0'
     return
-    
+
 
 def blktridiag_mul(A, B, v):
     r"""
@@ -226,20 +236,21 @@ def blktridiag_mul(A, B, v):
         N-1 blocks of nxm matrices in one
     """
     N, n, m = np.shape(A)
-    w = np.zeros([n*N, m])
+    w = np.zeros([n * N, m])
     w[0] = np.dot(A[0], v[0]) + np.dot(B[0].T, v[1])
-    w[-1] = np.dot(A[-1],v[-1]) + np.dot(B[-1], v[-2])
-    for k in xrange(1,N-1):
-        w[k] = np.dot(A[k], v[k]) + np.dot(B[k].T, v[k+1]) + np.dot(B[k-1], v[k-1])
-        
+    w[-1] = np.dot(A[-1], v[-1]) + np.dot(B[-1], v[-2])
+    for k in xrange(1, N - 1):
+        w[k] = np.dot(A[k], v[k]) + np.dot(B[k].T, v[k + 1]) + np.dot(B[k - 1], v[k - 1])
+
     return w
+
 
 def blkdiag_mul(A, v):
     """
     Block diagonal matrix multiplication with A_{i} as diagonal blocks.
     """
     m = np.shape(v)[-1]
-    w = np.array([np.dot(A[k],v[k]) for k in xrange(m)])
+    w = np.array([np.dot(A[k], v[k]) for k in xrange(m)])
     return w
 
 
@@ -248,24 +259,24 @@ def blkdiag_mul_t(A, v):
     Block diagonal matrix multiplication with A_{i}^{T} as diagonal blocks.
     """
     m = np.shape(v)[-1]
-    w = np.array([np.dot(A[k].T,v[k]) for k in xrange(m)])
+    w = np.array([np.dot(A[k].T, v[k]) for k in xrange(m)])
     return w
-    
+
 
 def process_hess(G, Qinv):
     """
     Creates block-tridiagonal Hessian matrix for Kalman Bucy smoother.
     """
-    N,n = np.shape(G)[:-1]
+    N, n = np.shape(G)[:-1]
     # off-diagonal block matrices
-    A = np.zeros([N-1,n,n])
+    A = np.zeros([N - 1, n, n])
     # diagonal block matrices
-    D = np.zeros([N,n,n])
+    D = np.zeros([N, n, n])
     D[-1] = Qinv[-1]
-    for k in xrange(N-1):
-        D[k] = Qinv[k-1] + np.dot(G[k].T,np.dot(Qinv[k],G[k]))
-    A = np.array([-np.dot(Qinv[k],G[k]) for k in xrange(N-1)])
-    
+    for k in xrange(N - 1):
+        D[k] = Qinv[k - 1] + np.dot(G[k].T, np.dot(Qinv[k], G[k]))
+    A = np.array([-np.dot(Qinv[k], G[k]) for k in xrange(N - 1)])
+
     return D, A
 
 
@@ -280,35 +291,36 @@ def process_grad(x, g, G, Qinv):
         \frac{\partial f}{\partial x_{j}} = Q_{k}^{-1}[x_{k}-G_{k}x_{k-1}-g_{k}] 
              -G_{k+1}^{T}Q_{k}^{-1}[x_{k+1}-G_{k+1}x_{k}-g_{k+1}]
     """
-    N,n = np.shape(G)[:-1]
-    grad = np.zeros([N,n])
-    x01 = x[1] - g[1] - np.dot(G[1],x[0])
-    xm1 = x[-1] - g[-1] - np.dot(G[-1],x[-2])
-    grad[0] = -np.dot(G[1].T,np.dot(Qinv[1],x01))
-    grad[-1] = np.dot(Qinv[-1],xm1)
-    for k in xrange(1,N-1):
-        xk1 = x[k+1] - g[k+1] - np.dot(G[k+1],x[k])
-        xk =  x[k] - g[k] - np.dot(G[k],x[k-1])
-        grad[k] = np.dot(Qinv[k],xk) - np.dot(G[k+1].T,np.dot(Qinv[k+1],xk1))
-        
+    N, n = np.shape(G)[:-1]
+    grad = np.zeros([N, n])
+    x01 = x[1] - g[1] - np.dot(G[1], x[0])
+    xm1 = x[-1] - g[-1] - np.dot(G[-1], x[-2])
+    grad[0] = -np.dot(G[1].T, np.dot(Qinv[1], x01))
+    grad[-1] = np.dot(Qinv[-1], xm1)
+    for k in xrange(1, N - 1):
+        xk1 = x[k + 1] - g[k + 1] - np.dot(G[k + 1], x[k])
+        xk = x[k] - g[k] - np.dot(G[k], x[k - 1])
+        grad[k] = np.dot(Qinv[k], xk) - np.dot(G[k + 1].T, np.dot(Qinv[k + 1], xk1))
+
     return grad
-    
+
 
 def kuhn_tucker_l1(mu, s, y, r, b, d, Bdia, Hdia, Hlow, pPos, pNeg):
     """
     Update step for KKT solver and l1 cost on observed variable
     """
     Hy = blktridiag_mul(Hdia, Hlow, y)
-    Bt_SmR = blkdiag_mul_t(Bdia, s-r)
+    Bt_SmR = blkdiag_mul_t(Bdia, s - r)
     By = blkdiag_mul(Bdia, y)
-    
-    F = np.array([ pPos - pNeg - b - By, 
-         pNeg*s - mu, 
-         r + s - 2*np.sqrt(2),
-         pPos*r - mu,
-         Hy + d + 0.5*Bt_SmR])
-    
+
+    F = np.array([pPos - pNeg - b - By,
+                  pNeg * s - mu,
+                  r + s - 2 * np.sqrt(2),
+                  pPos * r - mu,
+                  Hy + d + 0.5 * Bt_SmR])
+
     return F
+
 
 def l2l1_obj(x, z, g, h, G, H, Qinv, Rinv):
     """
@@ -318,26 +330,26 @@ def l2l1_obj(x, z, g, h, G, H, Qinv, Rinv):
         f(x_{k}) = \frac{1}{2}\sum |x-g(x)|_{Q^{-1}}^{2} + \sqrt(2)\sum|z-h(x)|_{R^{-1}}
     
     """
-    N,n = np.shape(x)
-    #chQ = np.linalg.cholesky(Qinv[0])
+    N, n = np.shape(x)
+    # chQ = np.linalg.cholesky(Qinv[0])
     chR = np.linalg.cholesky(Rinv[0])
-    zRes = z[0] - h[0] - np.dot(H[0],x[0])
+    zRes = z[0] - h[0] - np.dot(H[0], x[0])
     xRes = x[0] - g[0]
-    loss = np.sqrt(2)*np.linalg.norm(np.dot(chR,zRes),1)
-    loss += .5*np.dot(xRes.T,np.dot(Qinv[0],xRes)) #np.linalg.norm(np.dot(chQ,xRes),2)**2.
-    for k in xrange(1,N):
-        #chQ = np.linalg.cholesky(Qinv[k])
+    loss = np.sqrt(2) * np.linalg.norm(np.dot(chR, zRes), 1)
+    loss += .5 * np.dot(xRes.T, np.dot(Qinv[0], xRes))  # np.linalg.norm(np.dot(chQ,xRes),2)**2.
+    for k in xrange(1, N):
+        # chQ = np.linalg.cholesky(Qinv[k])
         chR = np.linalg.cholesky(Rinv[k])
-        zRes = z[k] - h[k] - np.dot(H[k],x[k])
+        zRes = z[k] - h[k] - np.dot(H[k], x[k])
         xRes = x[k] - g[k]
-        xRes = x[k] - g[k] - np.dot(G[k],x[k-1])
-        loss += np.sqrt(2)*np.linalg.norm(np.dot(chR,zRes),1)
-        loss += .5*np.dot(xRes.T,np.dot(Qinv[k],xRes)) #np.linalg.norm(np.dot(chQ,xRes),2)**2.
-    
-    return loss                            
+        xRes = x[k] - g[k] - np.dot(G[k], x[k - 1])
+        loss += np.sqrt(2) * np.linalg.norm(np.dot(chR, zRes), 1)
+        loss += .5 * np.dot(xRes.T, np.dot(Qinv[k], xRes))  # np.linalg.norm(np.dot(chQ,xRes),2)**2.
+
+    return loss
 
 
-def newton_step_l1(mu,s,y,r,b,d,BDia,HDia,HLow,pPos,pNeg):
+def newton_step_l1(mu, s, y, r, b, d, BDia, HDia, HLow, pPos, pNeg):
     r"""
     Inputs
     ------
@@ -364,50 +376,50 @@ def newton_step_l1(mu,s,y,r,b,d,BDia,HDia,HLow,pPos,pNeg):
     pNeg : float
         Second slack variable for L1 portion of objective
     """
-    N,n = np.shape(y)
+    N, n = np.shape(y)
     m = np.shape(b)[-1]
     if (np.min(s) < 0 or np.min(r) <= 0 or np.min(pPos) <= 0 or np.min(pNeg) <= 0):
         print 'L1_affine: initial s, r, pPlus, pMinus not all positive'
         return
-    rs = r*s
-    rpN = r*pNeg
-    spP = s*pPos
-    tinv = rs/(rpN+spP)
-    tinvir = s/(rpN+spP)
-    tinvis = r/(rpN+spP)
-    
+    rs = r * s
+    rpN = r * pNeg
+    spP = s * pPos
+    tinv = rs / (rpN + spP)
+    tinvir = s / (rpN + spP)
+    tinvis = r / (rpN + spP)
+
     # Create a new array for modified diagonal blocs
-    modCDiag = np.array([HDia[k]+np.dot(BDia[k].T,np.dot(np.diag(tinv[k]),BDia[k])) for k in xrange(N)])
-        
+    modCDiag = np.array([HDia[k] + np.dot(BDia[k].T, np.dot(np.diag(tinv[k]), BDia[k])) for k in xrange(N)])
+
     # Compute D(pPos) and D(pNeg)
-    
-    
+
+
     Cy = blktridiag_mul(HDia, HLow, y)
     By = blkdiag_mul(BDia, y)
-    
-    temp = s - np.sqrt(2) + tinv*(b + By + pPos) + mu*tinvis + tinvir*(2.*np.sqrt(2)*pPos-mu-pPos*s)
+
+    temp = s - np.sqrt(2) + tinv * (b + By + pPos) + mu * tinvis + tinvir * (2. * np.sqrt(2) * pPos - mu - pPos * s)
     BTtemp = blkdiag_mul_t(BDia, temp)
-    
+
     R5 = Cy + d + BTtemp
-    
+
     dy = -tridiag_solve_b(modCDiag, HLow, R5)
-    
+
     Bypdy = blkdiag_mul(BDia, y + dy)
-    
-    ds = tinv*(b + Bypdy - pPos) + tinvis*mu + tinvir*(2.*np.sqrt(2)*pPos-mu-pPos*s)
-    dr = 2.*np.sqrt(2) - r - s - ds
-    dpNeg = (mu - pNeg*ds)/s - pNeg
+
+    ds = tinv * (b + Bypdy - pPos) + tinvis * mu + tinvir * (2. * np.sqrt(2) * pPos - mu - pPos * s)
+    dr = 2. * np.sqrt(2) - r - s - ds
+    dpNeg = (mu - pNeg * ds) / s - pNeg
     dpPos = dpNeg + Bypdy + b + pNeg - pPos
-    
-    dpPos = np.reshape(dpPos, (N,m))
-    dpNeg = np.reshape(dpNeg, (N,m))
-    dy = np.reshape(dy, (N,n))
-    dr = np.reshape(dr, (N,m))
-    ds = np.reshape(ds, (N,m))
-    
+
+    dpPos = np.reshape(dpPos, (N, m))
+    dpNeg = np.reshape(dpNeg, (N, m))
+    dy = np.reshape(dy, (N, n))
+    dr = np.reshape(dr, (N, m))
+    ds = np.reshape(ds, (N, m))
+
     return dpPos, dpNeg, dr, ds, dy
-    
-    
+
+
 def tridiag_solve_b(C, A, r):
     """
     Solves block-tridiagonal system of equations, where C are the diagonal 
@@ -418,20 +430,20 @@ def tridiag_solve_b(C, A, r):
     ----------
     Algorithm 2.1 from https://arxiv.org/pdf/1303.1993.pdf
     """
-    N,n = np.shape(C)[:-1]
-    D = np.zeros([N,n,n])
-    s = np.zeros([N,n])
+    N, n = np.shape(C)[:-1]
+    D = np.zeros([N, n, n])
+    s = np.zeros([N, n])
     D[0] = C[0]
     s[0] = r[0]
-    for k in xrange(1,N):
-        D[k] = C[k] - np.dot(A[k-1].T,cho_solver(D[k-1],A[k-1]))
-        s[k] = r[k] - np.dot(A[k-1],cho_solver(D[k-1],s[k-1]))
-        
-    e = np.zeros([N,n])
-    e[-1] = cho_solver(D[-1],s[-1])
-    for k in xrange(N-1,0,-1):
-        e[k] = cho_solver(D[k],(s[k]-np.dot(A[k-1],e[k])))
-        
+    for k in xrange(1, N):
+        D[k] = C[k] - np.dot(A[k - 1].T, cho_solver(D[k - 1], A[k - 1]))
+        s[k] = r[k] - np.dot(A[k - 1], cho_solver(D[k - 1], s[k - 1]))
+
+    e = np.zeros([N, n])
+    e[-1] = cho_solver(D[-1], s[-1])
+    for k in xrange(N - 1, 0, -1):
+        e[k] = cho_solver(D[k], (s[k] - np.dot(A[k - 1], e[k])))
+
     return e
 
 
